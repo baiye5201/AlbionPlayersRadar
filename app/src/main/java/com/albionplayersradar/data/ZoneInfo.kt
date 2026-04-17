8a1602acc580c177b41d6f0dfcd4ef900f16c358
Gourav meena <67853722+GGourav@users.noreply.github.com>
2026-04-17 15:20:52 +0530

    Update ZoneInfo.kt

--- app/src/main/java/com/albionplayersradar/data/ZoneInfo.kt
+++ app/src/main/java/com/albionplayersradar/data/ZoneInfo.kt
@@ -5,3 +5,26 @@ data class ZoneInfo(
     val pvpType: String,
     val tier: Int
 )
+
+object ZonesDatabase {
+    private val zones = mapOf(
+        "THETFORD" to ZoneInfo("Thetford", "safe", 4),
+        "LYMHURST" to ZoneInfo("Lymhurst", "safe", 3),
+        "FORTSTERLING" to ZoneInfo("Fort Sterling", "safe", 2),
+        "MARTLOCK" to ZoneInfo("Martlock", "safe", 5),
+        "BRIDGEWATCH" to ZoneInfo("Bridgewatch", "safe", 6),
+        "CARLEON" to ZoneInfo("Caerleon", "black", 8),
+        "CAERLEON" to ZoneInfo("Caerleon Roads", "black", 8),
+        "ROAD" to ZoneInfo("Royal Roads", "red", 7),
+        "1000-1" to ZoneInfo("Yellow Zone", "yellow", 4),
+        "2000-1" to ZoneInfo("Red Zone", "red", 6),
+        "3000-1" to ZoneInfo("Black Zone", "black", 8)
+    )
+
+    fun getZone(zoneId: String): ZoneInfo? = zones[zoneId]
+    fun getPvpType(zoneId: String): String = zones[zoneId]?.pvpType ?: "safe"
+    fun isDangerous(zoneId: String): Boolean {
+        val pvp = getPvpType(zoneId)
+        return pvp == "black" || pvp == "red"
+    }
+}
